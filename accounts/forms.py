3057591b39fb16ed12c36b2155f04feade6f5f04
sinwoobang <sinwoobang@gmail.com>
2019-04-07 01:06:40 +0900

    Customized UsernameField to prevent to be input special characters

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm as BuiltInUserCreationForm, UsernameField
+from django.core.exceptions import ValidationError
 from django.forms import EmailField
 
 from accounts.models import User
+
+
+class JayparkUsernameField(UsernameField):
+    """Customized UsernameField to prevent to be input special characters."""
+    def validate(self, value):
+        super().validate(value)
+        if len(value) <= 5:
+            raise ValidationError('Username should be longer than 5 letters.')
+        elif not value.isalnum():
+            raise ValidationError('The type of username should consist of alphabets and numbers.')
 
 
 class UserCreationForm(BuiltInUserCreationForm):
@@ -15,7 +26,7 @@ class UserCreationForm(BuiltInUserCreationForm):
         model = User
         fields = ('username', 'email')
         field_classes = {
-            'username': UsernameField,
+            'username': JayparkUsernameField,
             'email': EmailField
         }
 

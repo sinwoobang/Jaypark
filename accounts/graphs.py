7fc1f532c0b35fe06139c5aaf88255b8ad71ebc4
sinwoobang <sinwoobang@gmail.com>
2019-04-06 23:00:32 +0900

    Added the field pk on Node User

--- accounts/graphs.py
+++ accounts/graphs.py
@@ -1,12 +1,13 @@
 from neomodel import (
     StructuredNode, RelationshipFrom, RelationshipTo, StringProperty,
-    UniqueIdProperty
+    UniqueIdProperty, IntegerProperty
 )
 
 
 class User(StructuredNode):
     """Node User"""
     uid = UniqueIdProperty()
+    pk = IntegerProperty(unique_index=True, required=True)
     username = StringProperty(unique_index=True, required=True)
 
     following = RelationshipTo('User', 'FOLLOWING')

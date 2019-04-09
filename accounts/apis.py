af2337778c01a6cb76193590de056551c9bb441d
sinwoobang <sinwoobang@gmail.com>
2019-04-09 13:04:33 +0900

    Added Login API and Register API for non-web environment

--- accounts/apis.py
+++ accounts/apis.py
@@ -3,14 +3,96 @@ import logging
 from json import JSONDecodeError
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login as auth_login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.db import transaction
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
+from accounts.forms import UserCreationForm
 from accounts.graphs import User as UserNode
 
 User = get_user_model()
 logger = logging.getLogger('debugging')
+
+
+@require_http_methods(['POST'])
+@csrf_exempt
+def login(request):
+    """
+    Login API for non-web environment.(Especially for testing.)
+    """
+    try:
+        body_data = json.loads(request.body)
+    except JSONDecodeError:
+        return JsonResponse({
+            'status': 'error',
+            'status_code': 'invalid_body',
+            'status_message': 'No json data exists.'
+        })
+
+    form = AuthenticationForm(data=body_data)
+    if not form.is_valid():
+        return JsonResponse({
+            'status': 'error',
+            'status_code': 'invalid_data',
+            'status_message': 'Please check your username or password.'
+        })
+    user = form.get_user()
+    auth_login(request, user)
+
+    return JsonResponse({
+        'status': 'success',
+        'status_code': '',
+        'status_message': '',
+        'contents': {'user': {'id': user.id, 'username': user.username}}
+    })
+
+
+@require_http_methods(['POST'])
+@csrf_exempt
+def register(request):
+    """
+    Register API for non-web environment.(Especially for testing.)
+    """
+    try:
+        body_data = json.loads(request.body)
+    except JSONDecodeError:
+        return JsonResponse({
+            'status': 'error',
+            'status_code': 'invalid_body',
+            'status_message': 'No json data exists.'
+        })
+
+    with transaction.atomic():  # Queries will be committed if the below codes run without error.
+        reg_form = UserCreationForm(data=body_data)  # register
+        if reg_form.is_valid():  # if get succeed to register then login the user.
+            reg_form.save()
+
+            username = body_data['username']
+            password = body_data['password1']
+            user = authenticate(request=request, username=username, password=password)
+            auth_login(request, user)
+
+            try:
+                user.create_node()
+            except Exception as e:
+                logger.error('FAIL TO CREATE A USER NODE FOR {u}, {e}'.format(u=username, e=e))
+
+            return JsonResponse({
+                'status': 'success',
+                'status_code': '',
+                'status_message': ''
+            })
+
+    logger.info(reg_form.errors)
+
+    return JsonResponse({
+        'status': 'error',
+        'status_code': 'invalid_data',
+        'status_message': 'Please check your input.'
+    })
 
 
 @require_http_methods(['POST'])

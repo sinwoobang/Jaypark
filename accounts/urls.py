f94e0def7699fff622e06a1c1541f77e5c462904
sinwoobang <sinwoobang@gmail.com>
2019-04-09 13:06:23 +0900

    Added url patterns to accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,11 +5,13 @@ from django.contrib.auth.urls import urlpatterns as default_urlpatterns
 from accounts import views, apis
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html')),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='account.login'),
     path('register/', views.register, name='account.register'),
 ]
 
 urlpatterns += [
+    path('api/login/', apis.login, name='account.api.login'),
+    path('api/register/', apis.register, name='account.api.register'),
     path('follow/', apis.follow, name='account.api.follow'),
 ]
 urlpatterns += default_urlpatterns  # Default Django supported views of accounts
